8428edbed60b25b1498321d2ef926a7a1b07e8d6
Adam Braun <mail.adam.braun@gmail.com>
2026-02-25 15:22:04 +0200

    Add types urls and fetch helper

--- src/lib/corpus.ts
+++ src/lib/corpus.ts
@@ -1,7 +1,24 @@
-import { fetchJson } from './fetcher'
-import type { Manifest } from './types'
-import { manifestUrl } from './urls'
+import { fetchJson, fetchText } from './fetcher'
+import type { Manifest, VerseArtifacts, VerseRef } from './types'
+import { graphDotUrl, manifestUrl, traceJsonUrl, traceTxtUrl } from './urls'
 
 export async function fetchManifest(): Promise<Manifest> {
   return fetchJson<Manifest>(manifestUrl())
+}
+
+export async function fetchVerseArtifacts(
+  ref: VerseRef
+): Promise<VerseArtifacts> {
+  const [traceJson, traceTxt, graphDot] = await Promise.all([
+    fetchJson(traceJsonUrl(ref)),
+    fetchText(traceTxtUrl(ref)),
+    fetchText(graphDotUrl(ref)),
+  ])
+
+  return {
+    traceJson,
+    traceTxt,
+    graphDot,
+    verseText: undefined,
+  }
 }
