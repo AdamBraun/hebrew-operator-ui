595574e4ac9653f97a9435f30e7067ab3b55f305
Adam Braun <mail.adam.braun@gmail.com>
2026-02-25 15:02:24 +0200

    Add routing and layout shell

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,31 @@
 import AppShell from '../layout/AppShell'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
   return (
     <AppShell>
-      <h1>Home Page</h1>
-      <p>Use a URL like /genesis/001/001 to view a verse route.</p>
+      <h1>Hebrew Operator VM - Pasuk Trace Corpus</h1>
+      <p>
+        This interface will present verse text, a readable execution trace, and
+        a rendered graph for each selected passage.
+      </p>
+      <ul>
+        <li>
+          <Link to="/genesis/001/001">Genesis 001:001</Link>
+        </li>
+        <li>
+          <Link to="/genesis/022/002">Genesis 022:002</Link>
+        </li>
+        <li>
+          <Link to="/exodus/003/014">Exodus 003:014</Link>
+        </li>
+        <li>
+          <Link to="/deuteronomy/006/004">Deuteronomy 006:004</Link>
+        </li>
+        <li>
+          <Link to="/psalms/023/001">Psalms 023:001</Link>
+        </li>
+      </ul>
     </AppShell>
   )
 }
