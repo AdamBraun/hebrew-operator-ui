2d867255e73005c9f09a83f92d30745afbc00703
Adam Braun <mail.adam.braun@gmail.com>
2026-02-25 15:04:47 +0200

    Implement shared layout shell

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import VersePage from './pages/VersePage'
 
 function AppRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/:book/:chapter/:verse" element={<VersePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
