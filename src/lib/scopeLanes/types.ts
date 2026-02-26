2ee406152f24738e7e6fe42104f8dad47076c17d
Adam Braun <mail.adam.braun@gmail.com>
2026-02-26 14:23:05 +0200

    Implement header interaction

---
+++ src/lib/scopeLanes/types.ts
@@ -0,0 +1,42 @@
+export type BoundaryKind =
+  | 'glue'
+  | 'glue_maqqef'
+  | 'cut_1'
+  | 'cut_2'
+  | 'cut_3'
+  | 'hard'
+  | 'unknown'
+
+export type VerseWord = {
+  index: number
+  text: string
+}
+
+export type BoundaryAfterWord = {
+  wordIndex: number
+  kind: BoundaryKind
+}
+
+export type LaneRank = 3 | 2 | 1
+
+export type LaneSpan = {
+  rank: LaneRank
+  startWord: number
+  endWord: number
+}
+
+export type ScopeLanesModel = {
+  ref: {
+    book: string
+    chapter3: string
+    verse3: string
+  }
+  words: VerseWord[]
+  boundariesAfter: BoundaryAfterWord[]
+  lanes: {
+    rank3: LaneSpan[]
+    rank2: LaneSpan[]
+    rank1?: LaneSpan[]
+  }
+}
+
